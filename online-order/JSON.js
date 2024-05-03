259a141a47c117e1fbb81f03caef874b599ec50b
Daria <113102018+dariiakurilenko@users.noreply.github.com>
2024-05-03 10:11:06 +0300

    Add files via upload

---
+++ online-order/JSON.js
@@ -0,0 +1,57 @@
+let json_order = 
+`{
+	"user":{
+		"name":"Павел",
+		"surname":"Волков",
+		"email":"p-volkoff@mail.ru",
+		"phone":"8(912)552-92-25"
+	},
+
+	"order_info":
+		{
+			"order_date":"07.01.2023",
+			"delivery_type":"По адресу",
+			"discount":"10%",
+			"total_price":"",
+			"address":{
+				"city":"Москва",
+				"street":"Белореченская улица",
+				"house":"31"
+			}
+		},
+	
+	"products":[
+		{
+			"articul":"RTLACB138001",
+			"name":"Носки из хлопка SUPIMA",
+			"brand":"UNIQLO",
+			"color":"Синий",
+			"season":"Мульти",
+			"price":399,
+			"quantity":3,
+			"cost":""
+		},
+		{
+			"articul":"MP002XM0984Y",
+			"name":"Брюки",
+			"brand":"O\'stin",
+			"color":"Черный",
+			"season":"Демисезон",
+			"price":3799,
+			"quantity":1,
+			"cost":""
+		},
+		{
+			"articul":"MP002XM0984Y",
+			"name":"Дубленка CLAUDE",
+			"brand":"Mango Man",
+			"color":"Коричневый",
+			"season":"Мульти",
+			"price":11190,
+			"quantity":1,
+			"cost":""
+		}
+	]
+}`
+
+
