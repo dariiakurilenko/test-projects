64ef10b5b6752617fa71dd0b2471e6375cf557c6
Daria <113102018+dariiakurilenko@users.noreply.github.com>
2024-05-03 16:38:00 +0300

    Add files via upload

---
+++ user profile/personal.js
@@ -0,0 +1,97 @@
+let personnel = [
+    {
+        authorization: {
+            login: 'user1',
+            password: '123'
+        },
+        info: {
+            id_personnel: 1, 
+            surname: 'Бородина', 
+            name: 'Ксения', 
+            patronymic: 'Алексеевна', 
+            post: 'Преподаватель', 
+            category: 'Соответствие занимаемой должности', 
+            level_edu: 'Высшее профессиональное ', 
+            experience_total: 21.3, 
+            experience_college: 14.5, 
+            email: 'BorodinKA@mail.ru',
+            img: 'image/img1.jpg'
+        }
+    },
+    {
+        authorization: {
+            login: 'user2',
+            password: '123'
+        },
+        info: {
+            id_personnel: 2, 
+            surname: 'Кошкина', 
+            name: 'Вера', 
+            patronymic: 'Николаевна', 
+            post: 'Преподаватель',  
+            category: 'Соответствие занимаемой должности', 
+            level_edu: 'Высшее профессиональное ', 
+            experience_total: 29.1, 
+            experience_college: 29.1, 
+            email: 'koshkova_v_n@mail.ru',
+            img: 'image/img2.jpg'
+        }
+    },
+    {
+        authorization: {
+            login: 'user3',
+            password: '123'
+        },
+        info: {
+            id_personnel: 3, 
+            surname: 'Рыбкина', 
+            name: 'Ольга', 
+            patronymic: 'Витальевна', 
+            post: 'Преподаватель', 
+            category: 'Соответствие занимаемой должности', 
+            level_edu: 'Высшее профессиональное ', 
+            experience_total: 21.3, 
+            experience_college: 14.5, 
+            email: 'RubkinaOlga1234@mail.ru',
+            img: 'image/img3.jpg'
+        }
+    },		
+    {
+        authorization: {
+            login: 'user4',
+            password: '123'
+        },
+        info: {
+            id_personnel: 4, 
+            surname: 'Маркова', 
+            name: 'Елизавета', 
+            patronymic: 'Андреевна', 
+            post: 'Преподаватель', 
+            category: 'Первая', 
+            level_edu: 'Высшее профессиональное ', 
+            experience_total: 19.11, 
+            experience_college: 16.5, 
+            email: 'markovaaa.el@gmail.com',
+            img: 'image/img4.jpg'
+        }
+    },
+    {
+        authorization: {
+            login: 'user5',
+            password: '123'
+        },
+        info: {
+            id_personnel: 5, 
+            surname: 'Ильчук', 
+            name: 'Виталина', 
+            patronymic: 'Георгиевна', 
+            post: 'Преподаватель', 
+            category: 'Высшая', 
+            level_edu: 'Высшее профессиональное ', 
+            experience_total: 36.1, 
+            experience_college: 19, 
+            email: 'il4uk.v@mail.ru',
+            img: 'image/img5.jpg'
+        }
+    }				
+];
